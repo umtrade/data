43bf4c093171a51cf8713866008afe66c13649f5
bit <52428773+umtrade@users.noreply.github.com>
2021-05-11 10:21:43 +0100

    Add files via upload

---
+++ js/loader-script.js
@@ -0,0 +1,12 @@
+
+
+var myVar;
+
+function myLoader() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
